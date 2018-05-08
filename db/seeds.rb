4ea796cb3daf228947cbf7713fb310f2e15fb753
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-05-08 18:38:48 -0500

    added products to db using seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+product = Product.new(name: "Basketball",
+                      price: 55,
+                      image_url: "https://images-na.ssl-images-amazon.com/images/I/91uZGOOjgUL._SL1500_.jpg",
+                      description: "Wilson evolution indoor basketball")
+product.save
+
+product = Product.new(name: "Football", 
+                      price: 35, 
+                      image_url: "https://images-na.ssl-images-amazon.com/images/I/51OM+tVJ6AL._AC_US436_QL65_.jpg",
+                      description: "Wilson the duke football")
+product.save
+
+product = Product.new(name: "Soccer Ball", 
+                      price: 125, 
+                      image_url: "https://images-na.ssl-images-amazon.com/images/I/71ByHz2foGL._SL1001_.jpg",
+                      description: "Adidas official match ball")
+product.save
+
+product = Product.new(name: "Baseball", 
+                      price: 17, 
+                      image_url: "https://images-na.ssl-images-amazon.com/images/I/81A%2BPIApvoL._SL1500_.jpg",
+                      description: "Rawlings official baseball")
+product.save
+
+product = Product.new(name: "Tennis Raquet", 
+                      price: 79, 
+                      image_url: "https://images-na.ssl-images-amazon.com/images/I/31mmUFItpjL.jpg",
+                      description: "Head Ti S6")
+product.save
+
+product = Product.new(name: "Nascar Helmet", 
+                      price: 5300, 
+                      image_url: "https://images-na.ssl-images-amazon.com/images/I/71w5AtMEWIL._SL1500_.jpg",
+                      description: "Dale Earnhardt Jr signed helmet")
+product.save
