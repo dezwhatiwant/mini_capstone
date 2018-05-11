d8ed6a82f27028f72c6dced322810eaadee33153
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-05-10 20:44:09 -0500

    added create, show, and update actions

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -8,6 +8,31 @@ class Api::ProductsController < ApplicationController
     product_id = params[:id]
     @product = Product.find(product_id)
     render 'show.json.jbuilder'
+  end
+
+  def create
+    @product = Product.new(
+                           name: params[:name],
+                           price: params[:price],
+                           image_url: params[:image_url],
+                           description: params[:description]
+                           )
+
+    @product.save
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    product_id = params[:id]
+    @product = Product.find(product_id)
+
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+
+    @recipe.save
+    render 'show.json.jbuilder'
     
   end
 end
--- runner.rb
+++ runner.rb
@@ -5,5 +5,31 @@ require 'unirest'
 # puts JSON.pretty_generate(response.body)
 
 # SHOW ACTION
-response = Unirest.get("http://localhost:3000/api/products/1")
-puts JSON.pretty_generate(response.body)
+# response = Unirest.get("http://localhost:3000/api/products/1")
+# puts JSON.pretty_generate(response.body)
+
+# CREATE ACTION
+# response = Unirest.post(
+#                         "http://localhost:3000/api/products",
+#                         parameters: {name: "Ping Pong Paddle",
+#                                      price: 350,
+#                                      image_url: "https://images-na.ssl-images-amazon.com/images/I/71KfnLiVfvL.jpg",
+#                                      description: "Killerspin Stilo7 SVR"
+#                                     }
+#                         )
+# puts JSON.pretty_generate(response.body)
+
+# UPDATE ACTION
+product_id = 2
+runner_params = {
+
+}
+
+
+
+
+
+
+
+
+
