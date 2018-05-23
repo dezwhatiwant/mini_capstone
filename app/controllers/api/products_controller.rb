cedc4147f2608371c7323592d07675f33dd754a5
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-05-22 20:18:13 -0500

    added search and filtering

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,25 @@
 class Api::ProductsController < ApplicationController
   def index
     @products = Product.all
+
+    search_term = params[:search]
+    if search_term
+      @products = @products.where("name iLike ? OR description iLike ?", "%#{search_term}%", "#{search_term}")
+    end
+    
+    
+    sort_attribute = params[:sort_by]
+    sort_order = params[:sort_order]
+
+    if sort_attribute && sort_order
+      @products = @products.order(sort_attribute => sort_order)
+    elsif sort_attribute
+      @products = @products.order(sort_attribute => :asc)
+    else
+      @products = @products.order(:id => :asc)
+    end
+
+    
     render 'index.json.jbuilder'
   end
 
