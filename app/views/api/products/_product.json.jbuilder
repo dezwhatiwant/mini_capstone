55f1364af2736cab5e02da628ea49ce1b578d853
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-05-23 19:03:21 -0500

    added gem seed dump, and added supplier id

--- app/views/api/products/_product.json.jbuilder
+++ app/views/api/products/_product.json.jbuilder
@@ -6,6 +6,7 @@ json.total product.total
 json.image_url product.image_url
 json.description product.description
 json.discounted product.is_discounted?
+json.supplier_id product.supplier_id
 
 json.formatted do
   json.price number_to_currency(product.price)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,13 @@
-product = Product.new(name: "Basketball",
-                      price: 55,
-                      image_url: "https://images-na.ssl-images-amazon.com/images/I/91uZGOOjgUL._SL1500_.jpg",
-                      description: "Wilson evolution indoor basketball")
-product.save
-
-product = Product.new(name: "Football", 
-                      price: 35, 
-                      image_url: "https://images-na.ssl-images-amazon.com/images/I/51OM+tVJ6AL._AC_US436_QL65_.jpg",
-                      description: "Wilson the duke football")
-product.save
-
-product = Product.new(name: "Soccer Ball", 
-                      price: 125, 
-                      image_url: "https://images-na.ssl-images-amazon.com/images/I/71ByHz2foGL._SL1001_.jpg",
-                      description: "Adidas official match ball")
-product.save
-
-product = Product.new(name: "Baseball", 
-                      price: 17, 
-                      image_url: "https://images-na.ssl-images-amazon.com/images/I/81A%2BPIApvoL._SL1500_.jpg",
-                      description: "Rawlings official baseball")
-product.save
-
-product = Product.new(name: "Tennis Raquet", 
-                      price: 79, 
-                      image_url: "https://images-na.ssl-images-amazon.com/images/I/31mmUFItpjL.jpg",
-                      description: "Head Ti S6")
-product.save
-
-product = Product.new(name: "Nascar Helmet", 
-                      price: 5300, 
-                      image_url: "https://images-na.ssl-images-amazon.com/images/I/71w5AtMEWIL._SL1500_.jpg",
-                      description: "Dale Earnhardt Jr signed helmet")
-product.save
+Supplier.create!([
+  {name: "Xcom", email: "beware.end", phone_number: "3130655555"},
+  {name: "Walmart", email: "wal@mart.com", phone_number: "5055456576"},
+  {name: "Amazon", email: "amazon@bezos.com", phone_number: "0917651234"}
+])
+Product.create!([
+  {name: "Basketball", price: "65.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/91uZGOOjgUL._SL1500_.jpg", description: "Wilson evolution indoor basketball", supplier_id: 1},
+  {name: "Soccer Ball", price: "125.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/71ByHz2foGL._SL1001_.jpg", description: "Adidas Official Match Ball", supplier_id: 1},
+  {name: "Baseball", price: "25.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/81A%2BPIApvoL._SL1500_.jpg", description: "Rawlings official baseball", supplier_id: 2},
+  {name: "Ping Pong Paddle", price: "350.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/71KfnLiVfvL.jpg", description: "Killerspin Stilo7 SVR", supplier_id: 3},
+  {name: "Hackey Sack", price: "8.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/51Y3zI3VGtL.jpg", description: "World Footbag Dirtbag Hacky Sack Footbag", supplier_id: 2},
+  {name: "Bow and Arrow", price: "41.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/71HCuH1yHbL._SL1500_.jpg", description: "Bow and Arrow with Quiver Set", supplier_id: 1}
+])
