a8692ca889f6a927eae080f021ebbb2aad76b068
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-05-29 21:11:21 -0500

    modified orders controller to calculte subtotal tax and total

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,22 +1,14 @@
 class Api::OrdersController < ApplicationController
 
   def create
-    product = Product.find(params[:product_id])
 
-    calculated_subtotal = product.price * params[:quantity].to_i
-    calculated_tax = calculated_subtotal * 0.09
-    calculated_total = calculated_subtotal + calculated_tax
 
     @order = Order.new(
                        user_id: current_user.id,
                        product_id: params[:product_id],
-                       quantity: params[:quantity],
-                       subtotal: calculated_subtotal,
-                       tax: calculated_tax,
-                       total: calculated_total
+                       quantity: params[:quantity]
                       )
     @order.save
-
     render 'show.json.jbuilder'
   end
 
