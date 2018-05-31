8e6f5f4d69969b47d229779f03013c890904d057
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-05-31 15:00:10 -0500

    added product_category model and objects within to make many to many associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180527161612) do
+ActiveRecord::Schema.define(version: 20180530021713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "images", force: :cascade do |t|
     t.string "url"
@@ -30,6 +36,13 @@ ActiveRecord::Schema.define(version: 20180527161612) do
     t.decimal "subtotal", precision: 9, scale: 2
     t.decimal "tax", precision: 9, scale: 2
     t.decimal "total", precision: 9, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,59 @@
-Supplier.create!([
-  {name: "Xcom", email: "beware.end", phone_number: "3130655555"},
-  {name: "Walmart", email: "wal@mart.com", phone_number: "5055456576"},
-  {name: "Amazon", email: "amazon@bezos.com", phone_number: "0917651234"}
-])
-Product.create!([
-  {name: "Basketball", price: "65.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/91uZGOOjgUL._SL1500_.jpg", description: "Wilson evolution indoor basketball", supplier_id: 1},
-  {name: "Soccer Ball", price: "125.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/71ByHz2foGL._SL1001_.jpg", description: "Adidas Official Match Ball", supplier_id: 1},
-  {name: "Baseball", price: "25.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/81A%2BPIApvoL._SL1500_.jpg", description: "Rawlings official baseball", supplier_id: 2},
-  {name: "Ping Pong Paddle", price: "350.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/71KfnLiVfvL.jpg", description: "Killerspin Stilo7 SVR", supplier_id: 3},
-  {name: "Hackey Sack", price: "8.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/51Y3zI3VGtL.jpg", description: "World Footbag Dirtbag Hacky Sack Footbag", supplier_id: 2},
-  {name: "Bow and Arrow", price: "41.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/71HCuH1yHbL._SL1500_.jpg", description: "Bow and Arrow with Quiver Set", supplier_id: 1}
-])
+# Supplier.create!([
+#   {name: "Xcom", email: "beware.end", phone_number: "3130655555"},
+#   {name: "Walmart", email: "wal@mart.com", phone_number: "5055456576"},
+#   {name: "Amazon", email: "amazon@bezos.com", phone_number: "0917651234"}
+# ])
+# Product.create!([
+#   {name: "Basketball", price: "65.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/91uZGOOjgUL._SL1500_.jpg", description: "Wilson evolution indoor basketball", supplier_id: 1},
+#   {name: "Soccer Ball", price: "125.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/71ByHz2foGL._SL1001_.jpg", description: "Adidas Official Match Ball", supplier_id: 1},
+#   {name: "Baseball", price: "25.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/81A%2BPIApvoL._SL1500_.jpg", description: "Rawlings official baseball", supplier_id: 2},
+#   {name: "Ping Pong Paddle", price: "350.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/71KfnLiVfvL.jpg", description: "Killerspin Stilo7 SVR", supplier_id: 3},
+#   {name: "Hackey Sack", price: "8.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/51Y3zI3VGtL.jpg", description: "World Footbag Dirtbag Hacky Sack Footbag", supplier_id: 2},
+#   {name: "Bow and Arrow", price: "41.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/71HCuH1yHbL._SL1500_.jpg", description: "Bow and Arrow with Quiver Set", supplier_id: 1}
+# ])
+
+Category.create(name: "Cheap")
+Category.create(name: "Expensive")
+Category.create(name: "Indoor")
+Category.create(name: "Outdoor")
+
+ProductCategory.create(category_id: 3, product_id: 8)
+ProductCategory.create(category_id: 3, product_id: 9)
+ProductCategory.create(category_id: 4, product_id: 1)
+ProductCategory.create(category_id: 4, product_id: 3)
+ProductCategory.create(category_id: 4, product_id: 4)
+ProductCategory.create(category_id: 4, product_id: 7)
+ProductCategory.create(category_id: 5, product_id: 1)
+ProductCategory.create(category_id: 5, product_id: 7)
+ProductCategory.create(category_id: 6, product_id: 3)
+ProductCategory.create(category_id: 6, product_id: 4)
+ProductCategory.create(category_id: 6, product_id: 8)
+ProductCategory.create(category_id: 6, product_id: 9)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
