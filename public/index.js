9ae18482e08b7d7980317b84b6f7a8614a206744
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-06-24 10:41:29 -0500

    adding vue in place of rails frontend

--- public/index.js
+++ public/index.js
@@ -1,13 +1,61 @@
 /* global Vue, VueRouter, axios */
 
-var HomePage = {
-  template: "#home-page",
+// var HomePage = {
+//   template: "#home-page",
+//   data: function() {
+//     return {
+//       message: "Welcome to Vue.js!"
+//     };
+//   },
+//   created: function() {},
+//   methods: {},
+//   computed: {}
+// };
+
+var ProductsIndexPage = {
+  template: "#products-index-page",
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      products: []
     };
   },
-  created: function() {},
+  created: function() {
+    axios
+      .get('/api/products')
+      .then(function(response) {
+        this.products = response.data;
+      }.bind(this));
+  },
+  methods: {},
+  computed: {}
+};
+
+var ProductsShowPage = {
+  template: "#products-show-page",
+  data: function() {
+    return {
+      product: {
+        id: "",
+        name: "",
+        price: "",
+        description: "",
+        supplier: {},
+        formatted: [
+          {
+            price: "",
+          }
+        ],
+        images: []
+      }
+    };
+  },
+  created: function() {
+    axios
+      .get("/api/products/" + this.$route.params.id)
+      .then(function(reponse) {
+        this.product = reponse.data;
+      }.bind(this));
+  },
   methods: {},
   computed: {}
 };
@@ -87,12 +135,57 @@ var LogoutPage = {
   }
 };
 
+var NewProduct = {
+  template: "#new-product",
+  data: function() {
+    return {
+      name: "",
+      price: "",
+      description: "",
+      supplier_id: ""
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        name: this.name,
+        price: this.price,
+        description: this.description,
+        supplier_id: this.supplier_id
+      };
+      axios
+        .post("/api/products", params)
+        .then(function(response) {
+          router.push("/");
+        })
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var ShoppingCart = {
+  template: "#shopping-cart",
+  data: function() {
+    return {
+    };
+
+  }
+};
+
+
 var router = new VueRouter({
   routes: [
-    { path: "/", component: HomePage },
+    { path: "/", component: ProductsIndexPage },
     { path: "/signup", component: SignupPage },
     { path: "/login", component: LoginPage },
-    { path: "/logout", component: LogoutPage }
+    { path: "/logout", component: LogoutPage },
+    { path: "/products", component: ProductsIndexPage },
+    { path: "/products/new", component: NewProduct },
+    { path: "/products/:id", component: ProductsShowPage }
   ],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
